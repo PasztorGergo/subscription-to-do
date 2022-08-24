603e7747e1ead45d1047f4acc2503045c7b094ab
PasztorGergo <pasztorg05@gmail.com>
2022-08-24 20:08:21 +0200

    task page

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -24,11 +24,14 @@ const Navbar = () => {
               </a>
             </Link>
             <nav className="space-x-2 ml-6 hidden lg:block">
-              <Link href="/">
+              <Link href="/pricing">
                 <a className={s.link}>Pricing</a>
               </Link>
               <Link href="/account">
                 <a className={s.link}>Account</a>
+              </Link>
+              <Link href="/">
+                <a className={s.link}>Tasks</a>
               </Link>
             </nav>
           </div>
